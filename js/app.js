ca9e2e10c2a0dae391d4b88d4f50d61250b7956b
Daniel Abrante <danielabrante@protonmail.com>
2024-03-25 17:31:13 -0300

    style: Add styles to dropdown-menu

--- js/app.js
+++ js/app.js
@@ -5,4 +5,25 @@ function scrollHeader() {
   else header.classList.remove('active-header');
 }
 
-window.addEventListener('scroll', scrollHeader);
+const menuItem = document.querySelectorAll('.menu-item');
+
+menuItem.forEach(item => {
+  const navLink = item.querySelector(".nav-link-desktop");
+  const dropMenuItem = item.querySelector('.dropdown-menu');
+  const allDropDownMenu = document.querySelectorAll('.dropdown-menu');
+
+  navLink.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    allDropDownMenu.forEach(dropDownMenuItem => {
+      if (dropDownMenuItem == dropMenuItem) return;
+      dropDownMenuItem.classList.remove('active-dropdown-menu');
+    });
+
+    dropMenuItem.classList.add('active-dropdown-menu');
+  })
+
+  dropMenuItem.addEventListener('mouseleave', () => {
+    dropMenuItem.classList.remove('active-dropdown-menu');
+  })
+});
